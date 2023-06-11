4b722d1171f18b49c73b89045ab50bd608feef07
mp30028 <mp30028@gmail.com>
2023-06-11 20:58:29 +0100

    updated shell script with setup of env vars

--- setup-sops-and-age/scripts/cp_priv_key_to_sopsage_container.sh
+++ setup-sops-and-age/scripts/cp_priv_key_to_sopsage_container.sh
@@ -1,1 +1,2 @@
 docker cp $SOPS_AGE_KEY_FILE sopsage:/sopsage/configs/dev-server-1/age-key.private
+docker exec -i sopsage /bin/bash -c "export $SOPS_AGE_KEY_FILE=/sopsage/configs/dev-server-1/age-key.private"
